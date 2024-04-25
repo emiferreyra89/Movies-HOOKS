45bde087117d54701f54fe50b83ffd2358ba4d9a
emiferreyra89 <ferreyra89@gmail.com>
2024-04-25 18:02:54 -0300

    Subo el trabajo al repositorio remoto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SideBar from './components/SideBar.jsx';
+import './assets/css/app.css'
+
+function App() {
+  return (
+    <>
+      <div id="wrapper">
+          <SideBar />
+        </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ContentWrapper.jsx
@@ -0,0 +1,19 @@
+import TopBar from './TopBar';
+import ContentRowTop from './ContentRowTop';
+import Footer from './Footer';
+function ContentWrapper(){
+    return (
+        <>
+            {/*<!-- Content Wrapper -->*/}
+            <div id="content-wrapper" className="d-flex flex-column">
+                {/*<!-- Main Content -->*/}
+                <div id="content">
+                    <TopBar />
+                    <ContentRowTop />
+                    <Footer />
+                </div>
+            </div>    
+        </>
+    )
+}
+export default ContentWrapper;
